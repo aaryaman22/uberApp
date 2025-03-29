25226af653fd623c4bc1c2dea0b9793b32f0b2e9
aaryaman22 <aaryaman1436.be20@chitkara.edu.in>
2025-03-30 05:01:45 +0530

    ENTITY BASIC STRUCTURE

---
+++ src/main/java/com/project/uber/uberApp/UberAppApplication.java
@@ -0,0 +1,17 @@
+package com.project.uber.uberApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAppApplication.class, args);
+	}
+
+}
+
+//term for not analized keywords
+//match for analized text
+//aggr keyword pr chalte hai and all pr text pr nhi chalte hai
