fb69aa859e39ab3bcc19e3cf1f7843d30a44f619
aaryaman22 <aaryaman1436.be20@chitkara.edu.in>
2025-04-01 00:48:52 +0530

    fixing POM.xml for stop actively removing Lombok from the compilation process, even though it was in your dependencies.

--- src/main/java/com/project/uber/uberApp/configs/MapperConfig.java
+++ src/main/java/com/project/uber/uberApp/configs/MapperConfig.java
@@ -30,7 +30,5 @@ public class MapperConfig {
 
 
         return mapper;
-
-        return new ModelMapper();
     }
 }
--- src/main/java/com/project/uber/uberApp/strategies/impl/DriverMatchingHighestRatedDriverStrategy.java
+++ src/main/java/com/project/uber/uberApp/strategies/impl/DriverMatchingHighestRatedDriverStrategy.java
@@ -4,9 +4,11 @@ import com.project.uber.uberApp.dto.RideRequestDto;
 import com.project.uber.uberApp.entities.Driver;
 import com.project.uber.uberApp.entities.RideRequest;
 import com.project.uber.uberApp.strategies.DriverMatchingStrategy;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class DriverMatchingHighestRatedDriverStrategy implements DriverMatchingStrategy {
     @Override
     public List<Driver> findMatchingDriver(RideRequest rideRequest) {
