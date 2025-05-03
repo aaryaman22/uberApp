6aa085136fec8de9f9741647ef91718d4695a672
aaryaman22 <aaryaman1436.be20@chitkara.edu.in>
2025-05-04 04:27:49 +0530

    P11 redis and RedisCacheManager impl

--- src/main/java/com/project/uber/uberApp/dto/UserDto.java
+++ src/main/java/com/project/uber/uberApp/dto/UserDto.java
@@ -5,12 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
+public class UserDto implements Serializable {
 
     private Long id;
     private String name;
